c57c486e9b92970077ea09f56aacaea621b86708
klee <klee@awninsurance.com>
2020-10-27 17:15:20 +1000

    hex-clock init

---
+++ script.js
@@ -0,0 +1,17 @@
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    var hex = '#' + h + m + s;
+    document.getElementById('time').innerHTML = hex;
+    document.getElementsByTagName('html')[0].style.backgroundColor = hex;
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
+}
